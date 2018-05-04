b09aad9bf679af9eae91c3a984b0d6dc49febd1d
babang <bagusprabangkoro@gmail.com>
2018-05-04 10:18:26 +0700

    [Kulina] *finishing the footer styling; *add responsive nav background; *fix card button;

--- src/js/index.js
+++ src/js/index.js
@@ -1,1 +1,27 @@
 console.log('compiled');
+var nav = $('nav');
+
+//on load & scroll activate nav bg
+$(window).on('load scroll', function () {
+  var curPos = $(this).scrollTop();
+  var limHeight = parseFloat($('.jumbotron h1').css('padding-top'));
+  if (curPos < limHeight) {
+    nav.removeClass('bg-show');
+  } else {
+    nav.addClass('bg-show');
+  }
+});
+
+//on hamburger icon click activate nav bg
+$('.navbar-toggler').on('click', function () {
+  var curPos = $(window).scrollTop();
+  var limitHeight = parseFloat($('.jumbotron').css('padding-top'));
+  // below limit: hide bg
+  if (curPos < limitHeight) {
+    if (nav.hasClass('bg-show')) {
+      nav.removeClass('bg-show');      
+    } else {
+      nav.addClass('bg-show');
+    }
+  }
+})
